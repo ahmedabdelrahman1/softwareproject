86439e335f63407f8df9ec0d2e12bceb2088b193
Bassel196 <125680928+Bassel196@users.noreply.github.com>
2023-10-18 23:14:35 +0200

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,6 @@
                 <a class="navbar-brand fw-bold" href="index.html">
                     <img class="logo" src="static/assets/logo.jpg">7GES
                 </a>
-                <a class="btn btn-primary" href="courses.php">Sign In</a>
+                <a class="btn btn-primary" href="courses.php">Sign up</a>
             </div>
         </nav>
