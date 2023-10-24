4803e52b98e12e010c78755d9d0fc11586d0ccc3
ahmedabdelrahman1 <ahmedgaber1576@gmail.com>
2023-10-24 17:08:11 +0300

    add the delete course function

--- classcourse.php
+++ classcourse.php
@@ -1,6 +1,8 @@
 <?php
+require './classsection.php';
 class course {
     public static $alerts=[];
+    
     public static function connect(){
         $conn=new PDO("mysql:host=localhost;dbname=miu","root","");
         return $conn;
@@ -22,6 +24,37 @@ class course {
         return $fetch;
     }
 
+    public static function delete($courseID) {
+        $conn = course::connect();
 
+        // Check if the course exists
+        $checkQuery = $conn->prepare("SELECT * FROM course_table WHERE ID = ?");
+        $checkQuery->execute(array($courseID));
+
+        if ($checkQuery->rowCount() > 0) {
+            // Course found, proceed with deletion
+            
+            
+            // Reuse the delete function from the section class
+            $sections = section::selectByCourse($courseID);
+
+        if (count($sections) > 0) {
+            foreach ($sections as $value) {
+                section::delete($value['ID']);
+            }
+        }
+            
+            // Delete the course record from course_table
+            $deleteQuery = $conn->prepare("DELETE FROM course_table WHERE ID = ?");
+            if ($deleteQuery->execute(array($courseID))) {
+                course::$alerts[] = "Course and related sections deleted successfully.";
+            } else {
+                course::$alerts[] = "Failed to delete the course.";
+            }
+        } else {
+            // Course not found
+            course::$alerts[] = "Course not found.";
+        }
+    }
 }
 ?>
