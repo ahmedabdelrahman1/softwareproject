47c3ab4785f61b2a203037f18f6f83459e98b165
ArwaaMamdoh23 <arwaa2110478@miuegypt.edu.eg>
2023-10-26 00:09:50 +0300

    payment

---
+++ payment.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="payment.css">
+
+</head>
+<body>
+
+<div class="container">
+
+    <form action="">
+
+        <div class="row">
+
+            <div class="col">
+
+                <h3 class="title">Payment</h3>
+
+                <div class="inputBox">
+                    <span>Full name :</span>
+                    <input type="text" placeholder="Enter your name">
+                </div>
+                <div class="inputBox">
+                    <span>email :</span>
+                    <input type="email" placeholder="Enter your email">
+                </div>
+                <div class="inputBox">
+                    <span>address :</span>
+                    <input type="text" placeholder=" Enter your address detailes">
+                </div>
+                <div class="inputBox">
+                    <span>Governorate :</span>
+                    <input type="text" placeholder="Enter your governorate name">
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                        <span>City :</span>
+                        <input type="text" placeholder="Enter Your city name">
+                    </div>
+                </div>
+                <div class="inputBox">
+                    <span>Telephone number</span>
+                    <input type="number"placeholder="Enter your telephone number">
+                </div>
+                    <div class="inputBox">
+                        <span>Zip code :</span>
+                        <input type="text" placeholder="Enter yor city Zip code">
+                    </div>
+                </div>
+
+           
+
+            <div class="col">
+
+                <h3 class="title">Payment</h3>
+
+                <div class="inputBox">
+                    <span>cards accepted :</span>
+                    <img src="images/card_img.png" alt="">
+                </div>
+                <div class="inputBox">
+                    <span>Card Name  :</span>
+                    <input type="text" placeholder="Enter your card holder name ">
+                </div>
+                <div class="inputBox">
+                    <span>Credit card number :</span>
+                    <input type="number" placeholder="Enter creidt card number">
+                </div>
+                <div class="inputBox">
+                    <span>CVV :</span>
+                    <input type="text" placeholder="Enter Cvv number">
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                        <span>Expiry year :</span>
+                        <input type="number" placeholder="year">
+                    </div>
+                    <div class="inputBox">
+                        <span>Expiry month :</span>
+                        <input type="text" placeholder="month ">
+                    </div>
+            
+                </div>
+
+            </div>
+    
+        </div>
+
+        <input type="submit" value="proceed to checkout" class="submit-btn">
+
+    </form>
+
+</div>    
+    
+</body>
+</html>
