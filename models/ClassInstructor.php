185eeaafc48d18bdf8e2dd44c7a4f16ef39f9b91
ahmedabdelrahman1 <ahmedgaber1576@gmail.com>
2023-12-19 02:50:15 +0200

    no the instractor can grade the assignments

--- models/ClassInstructor.php
+++ models/ClassInstructor.php
@@ -1,8 +1,23 @@
 <?php
 require_once("Models.php");
 require_once("classUser.php");
+require_once("assignment_class.php");
 class Instructor extends User {
 
+    private $instructorID;
 
+    public function __construct($id = "")
+    {
+        $this->db = $this->connect();
+
+        $this->instructorID = $id;
+    }
+
+    public function gradeassignments ($assignment_id,$grade)
+    {
+           $objectassignment=new assignment();
+
+           $objectassignment->grade($assignment_id,$grade);
+    }
 }
 ?>
