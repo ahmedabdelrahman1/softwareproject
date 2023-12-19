d5398dd5ef3166a3a0901bea9421fc2c77977acd
ArwaaMamdoh23 <arwaa2110478@miuegypt.edu.eg>
2023-12-19 02:06:40 +0200

    edits

--- views/chat.php
+++ views/chat.php
@@ -4,45 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat  Form</title>
+    <link rel="stylesheet" href="chat.css">
 </head>
 <body>
     <h1>Chat Form</h1>
 
     <form action="process_contact_form.php" method="post">
-        <label for="sender_name">Your Name:</label>
+        <label for="sender_name"> Enter Your Name:</label>
         <input type="text" name="sender_name" required>
 
-        <label for="email">Your Email:</label>
+        <label for="email"> Enter Your Email:</label>
         <input type="email" name="email" required>
 
-        <label for="message">Message:</label>
+        <label for="message"> Enter your Message:</label>
         <textarea name="message" rows="4" required></textarea>
 
         <input type="submit" value="Submit">
     </form>
 </body>
 </html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
-</head>
-<body>
-    <h1>Chat Form</h1>
 
-    <form action="process_contact_form.php" method="post">
-        <label for="sender_name">Your Name:</label>
-        <input type="text" name="sender_name" required>
 
-        <label for="email">Your Email:</label>
-        <input type="email" name="email" required>
-
-        <label for="message">Message:</label>
-        <textarea name="message" rows="4" required></textarea>
-
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
