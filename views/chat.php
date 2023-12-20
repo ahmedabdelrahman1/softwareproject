79731f350ddc19f947559b292ffa5de2c0e4511e
ArwaaMamdoh23 <arwaa2110478@miuegypt.edu.eg>
2023-12-20 05:56:13 +0200

    edits

--- views/chat.php
+++ views/chat.php
@@ -4,25 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat Form</title>
-    <link rel="stylesheet" href="chat.css">
+    <link rel="stylesheet" href="../../public/css/chat.css">
 </head>
 <body>
 
-    <!-- Button to toggle chat form visibility -->
-    <button id="toggleChat">Toggle Chat</button>
 
     <!-- Chat Form -->
     <form id="chatForm" action="process_contact_form.php" method="post">
-        <label for="sender_name"> Enter Your Name:</label>
+        <label for="sender_name"><br> Enter Your Name:</label><br>
         <input type="text" name="sender_name" required>
 
-        <label for="email"> Enter Your Email:</label>
+        <label for="email"><br> Enter Your Email:</label><br>
         <input type="email" name="email" required>
 
-        <label for="message"> Enter your Message:</label>
+        <label for="message"><br> Enter your Message:</label><br>
         <textarea name="message" rows="4" required></textarea>
-
-        <input type="submit" value="Submit">
+            <br>
+        <input type="submit" value="Submit"><br>
     </form>
 
     <script src="chat.js"></script>
